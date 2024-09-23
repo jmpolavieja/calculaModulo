6360942356e31dee8782bb5bd18231fe366d5023
Josemaría García de Polavieja Ferre <jgf@altair.edu.es>
2024-09-23 22:16:32 +0200

    primer commit solucion

---
+++ main.js
@@ -0,0 +1,27 @@
+import { add, subtract, multiply, divide } from './operaciones.js';
+
+function calculate(operation) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+
+  switch (operation) {
+    case 'add':
+      result = add(num1, num2);
+      break;
+    case 'subtract':
+      result = subtract(num1, num2);
+      break;
+    case 'multiply':
+      result = multiply(num1, num2);
+      break;
+    case 'divide':
+      result = divide(num1, num2);
+      break;
+  }
+
+  document.getElementById("result").innerText = `Resultado: ${result}`;
+}
+
+// Hacer las funciones globales para poder usar onclick en el HTML
+window.calculate = calculate;
